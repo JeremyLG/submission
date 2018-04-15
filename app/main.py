3d814988748a8d9516cf1e3c7295ebaac67fdfd5
JeremyLG1 <jeremy.le-gall@hotmail.fr>
2018-04-15 15:24:25 +0200

    cert

--- app/main.py
+++ app/main.py
@@ -5,5 +5,5 @@ from app import app
 
 if __name__ == '__main__':
     print("yooo")
-    context = ('/home/ubuntu/ssl/cert.crt', '/home/ubuntu/ssl/key.key')
+    context = ('/home/ubuntu/ssl/foo.cert', '/home/ubuntu/ssl/key.key')
     app.run(host='0.0.0.0', ssl_context=context, threaded=True, debug=True)
