9fae3ab970ec3972364f0b7d0583b8b3ca30bc3c
JeremyLG1 <jeremy.le-gall@hotmail.fr>
2018-04-15 15:19:32 +0200

    port 5000

--- app/main.py
+++ app/main.py
@@ -6,4 +6,4 @@ from app import app
 if __name__ == '__main__':
     print("yooo")
     context = ('/home/ubuntu/ssl/cert.crt', '/home/ubuntu/ssl/key.key')
-    app.run(host='0.0.0.0', port=80, ssl_context=context, threaded=True, debug=True)
+    app.run(host='0.0.0.0', ssl_context=context, threaded=True, debug=True)
