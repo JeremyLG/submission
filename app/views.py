f3bb8e0cc1123bb6f3d70cc243a4bc7e199f7640
JeremyLG1 <jeremy.le-gall@hotmail.fr>
2018-04-16 16:45:22 +0200

    plots matrix

--- app/views.py
+++ app/views.py
@@ -119,6 +119,22 @@ def scores():
     competitions = Competition.query.all()
     return render_template('scores.html', competitions=competitions)
 
+@app.route('/plot')
+def build_plot():
+
+    img = io.BytesIO()
+
+    y = [1,2,3,4,5]
+    x = [0,2,1,3,4]
+    plt.plot(x,y)
+    plt.savefig(img, format='png')
+    img.seek(0)
+
+    plot_url = base64.b64encode(img.getvalue()).decode()
+
+    return '<img src="data:image/png;base64,{}">'.format(plot_url)
+
+
 @app.route('/_get_submissions', methods=['POST'])
 @login_required
 def get_submissions():
@@ -141,7 +157,7 @@ def get_submissions():
 
         rows = ""
         for d in dates:
-            row = '{{"c":[{{"v":"Date({0},{1},{2},{3},{4},{5})"}}'.format(d.year, d.month - 1, d.day, d.hour + 1, d.minute, d.second)
+            row = '{{"c":[{{"v":"Date({0},{1},{2},{3},{4},{5})"}}'.format(d.year, d.month - 1, d.day, d.hour, d.minute, d.second)
             for u in user_ids:
                 s = submissions.filter(Submission.submitted_on==d).filter(Submission.user_id==u)
                 if s.count() > 0:
